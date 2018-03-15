8d0b69c8e9a5cb614b62ffa614acdd0a7026d259
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-15 10:47:57 -0400

    removing test dependencies

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "Concentration",
             dependencies: []),
-        .testTarget(
-            name: "ConcentrationTests",
-            dependencies: ["Concentration"]),
     ]
 )
